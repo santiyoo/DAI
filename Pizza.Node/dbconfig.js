82c2d95155b70ae415dfeb9630d3b9e52d206430
santiyoo <yoopy2705@gmail.com>
2022-05-23 09:42:36 -0300

    ...

---
+++ Pizza.Node/dbconfig.js
@@ -0,0 +1,12 @@
+const config = {
+    user: 'Pizzas',
+    password: 'Pizzas',
+    server: '',
+    database: 'DAI-Pizzas',
+    options: {
+        trustServerCertificate: true,
+        trustedConnection: true
+    }
+}
+
+export default config;
