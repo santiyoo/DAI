d565f4794d00f2b359d016ea2da0d9da99bff1b6
joacoabraldes <abraldesjoaquin@gmail.com>
2022-05-23 10:36:55 -0300

    v5

--- Pizza.Node/index.js
+++ Pizza.Node/index.js
@@ -7,8 +7,5 @@ console.log(todo);
 let pizzaId = await srv.getById(1);
 console.log(pizzaId);
 
-<<<<<<< HEAD
-=======
 let pizzaBorrada = await srv.deleteById(1);
 console.log(pizzaBorrada);
->>>>>>> 10d75dc9b590e30a747894a97346641cf2671c0c
