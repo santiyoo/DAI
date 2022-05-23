384197014616fb58821cc6d3f2ef9307955eda4a
joacoabraldes <abraldesjoaquin@gmail.com>
2022-05-23 09:59:59 -0300

    v4

--- Pizza.Node/src/services/pizzas-services.js
+++ Pizza.Node/src/services/pizzas-services.js
@@ -12,11 +12,13 @@ class PizzaService {
 
             let pool = await sql.connect(config);
             let result = await pool.request()
-                                            .input(pid)
-                                            .query("SELECT TOP 2 * FROM Pizzas");
+                                            .input('pId', sql.Int, id)
+                                            .query('SELECT TOP 2 * FROM Pizzas WHERE id = @pId');
+            returnEntity = result.recordsets[0][0]
 
         } catch (error) {
-            
+            console.log(error)
         }
+        returnEntity
     } 
 }
