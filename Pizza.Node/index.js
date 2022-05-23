410335630a55cac2ae5b736ff114f09d9409588e
joacoabraldes <abraldesjoaquin@gmail.com>
2022-05-23 10:11:18 -0300

    v3

--- Pizza.Node/index.js
+++ Pizza.Node/index.js
@@ -1,7 +1,13 @@
 import sql from 'mssql';
 import config from "./dbconfig.js"
+import PizzaService from "./src/services/pizzas-services.js"
 
-let pool = await sql.connect(config);
-let result = await pool.request().query("SELECT TOP 2 * FROM Pizzas");
+// let pool = await sql.connect(config);
+// let result = await pool.request().query("SELECT TOP 2 * FROM Pizzas");
 
-console.log(result.recordsets.length);
+// console.log(result.recordsets.length);
+
+let data;
+
+data = await PizzaService.getById(2);
+console.log(data)
